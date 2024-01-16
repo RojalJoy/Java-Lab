2ca0a82928aad311b29aa1e16690611d51b4e2c1
RojalJoy <141493899+RojalJoy@users.noreply.github.com>
2024-01-16 09:24:14 +0530

    Update Lab_Exercise_7.java

--- Lab_Exercise_7.java
+++ Lab_Exercise_7.java
@@ -1,3 +1,21 @@
-public class Lab_Exercise_7 {
-    
+
+interface Store {
+    String performAction(String member);
+}
+
+interface StoreMember {
+    int MemberId(int Id);
+}
+
+public class LabExercise_7 {
+    public static void main(String[] args) {
+
+        Store welcomeMember = (member) -> "Welcome to PC game site, " + member + "!";
+        Store loginInMember = (member) -> member + " has loged in.";
+        StoreMember LoginId = (Id) -> Id;
+
+        System.out.println(welcomeMember.performAction("Riya"));
+        System.out.println(loginInMember.performAction("Riya"));
+        System.out.println(LoginId.MemberId(21));
+    }
 }
